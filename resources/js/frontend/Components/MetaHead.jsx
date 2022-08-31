597ef0ddf54c07febf3b5cb7c0170e5c653cc19c
Himanshu Batra <himanshu@batra.io>
2022-08-31 14:47:54 -0400

    WiP

--- resources/js/frontend/Components/MetaHead.jsx
+++ resources/js/frontend/Components/MetaHead.jsx
@@ -1,11 +1,12 @@
-import { InertiaHead } from '@inertiajs/inertia-react';
 import React from 'react';
+import { InertiaHead } from '@inertiajs/inertia-react';
+
 
 function MetaHead({ title, name, description }) {
     return (
         <InertiaHead>
             <title>{title}</title>
-            <meta name={name} content={description} />
+            <meta name={name} content={desc} />
         </InertiaHead>
     )
 }
