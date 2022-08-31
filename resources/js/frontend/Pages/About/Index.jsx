0e13280bfb2dc6a2c06bd4e862ecf8882bba6429
Himanshu Batra <himanshu@batra.io>
2022-08-31 15:02:09 -0400

    WiP

--- resources/js/frontend/Pages/About/Index.jsx
+++ resources/js/frontend/Pages/About/Index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../../Layout'
+import Layout from '../../Layout/Index'
 import MetaHead from '../../Components/MetaHead';
 
 function About() {
--- resources/js/frontend/Pages/Home/Index.jsx
+++ resources/js/frontend/Pages/Home/Index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import Layout from '../../Layout'
+import Layout from '../../Layout/Index'
 import MetaHead from '../../Components/MetaHead'
 
 function Home() {
