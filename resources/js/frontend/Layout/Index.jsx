14f12a228d29a74b7dd4939fe53c449047d409df
Himanshu Batra <himanshu@batra.io>
2022-08-31 15:05:23 -0400

    WiP

--- resources/js/frontend/Layout/Index.jsx
+++ resources/js/frontend/Layout/Index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './Header'
-import Footer from './Footer'
+import Header from './Header/Index'
+import Footer from './Footer/Index'
 
 function Layout({ children }) {
     return (
